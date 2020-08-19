f7533d5757d85c57e24aae5b3e74884f0a29f009
xacer <chacecaven@outlook.com>
2020-08-19 14:25:52 -0400

    Update xgl.js

--- webgl/xgl.js
+++ webgl/xgl.js
@@ -8,6 +8,8 @@ var XGL;
 	
 	var gl, canvas;
 	
+	var getCrossProduct = function
+	
 	var loadGL = function(c) {
 		XGL.canvas = c;
 		XGL.width = c.width;
@@ -136,8 +138,111 @@ var XGL;
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, textureObject.width, textureObject.height, 0, gl.RGB, gl.UNSIGNED_BYTE, textureObject.data);
 		this.setUniform(uniformName, "1i", n);
 	};
+	
+	var WorldEditor = function () {
+		this.nodes = [];
+		this.elements = [];
+		this.colors = [];
+	};
+	
+	WorldEditor.prototype.addQuad = function(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, r, g, b) {
+		var index0 = this.nodes.length / 3.0;
+		
+		var newNodes = [
+			x1, y1, z1,
+			x2, y2, z2,
+			x3, y3, z3,
+			x4, y4, z4
+		];
+		
+		var newColors = [
+			r, g, b, 1.0,
+			r, g, b, 1.0,
+			r, g, b, 1.0,
+			r, g, b, 1.0
+		];
+		
+		var newIndex = [
+			index0 + 0, index0 + 1, index0 + 2,
+			index0 + 0, index0 + 2, index0 + 3
+		];
+		
+		this.nodes = this.nodes.concat(newNodes);
+		this.elements = this.elements.concat(newNodes);
+		this.colors = this.colors.concat(newColors);
+	};
+	
+	WorldEditor.prototype.addTri = function(x1, y1, z1, x2, y2, z2, x3, y3, z3, r, g, b) {
+		var index0 = this.nodes.length / 3.0;
+		
+		var newNodes = [
+			x1, y1, z1,
+			x2, y2, z2,
+			x3, y3, z3
+		];
+		
+		var newColors = [
+			r, g, b,
+			r, g, b,
+			r, g, b
+		];
+		
+		var newIndex = [
+			index0 + 0, index0 + 1, index0 + 2
+		];
+		
+		this.nodes = this.nodes.concat(newNodes);
+		this.elements = this.elements.concat(newNodes);
+		this.colors = this.colors.concat(newColors);
+	};
+	
+	WorldEditor.prototype.addBox = function(x, y, z, w, h, d, faceColors) {
+		this.addQuad(
+			x - w/2, y - h/2, z - d/2,
+			x - w/2, y - h/2, z + d/2,
+			x - w/2, y + h/2, z + d/2,
+			x - w/2, y + h/2, z - d/2, 
+			faceColors[0][0], faceColors[0][1], faceColors[0][2]);
+		this.addQuad(
+			x + w/2, y - h/2, z - d/2,
+			x + w/2, y - h/2, z + d/2,
+			x + w/2, y + h/2, z + d/2,
+			x + w/2, y + h/2, z - d/2, 
+			faceColors[1][0], faceColors[1][1], faceColors[1][2]);
+		this.addQuad(
+			x - w/2, y - h/2, z - d/2,
+			x - w/2, y - h/2, z + d/2,
+			x + w/2, y - h/2, z + d/2,
+			x + w/2, y - h/2, z - d/2, 
+			faceColors[2][0], faceColors[2][1], faceColors[2][2]);
+		this.addQuad(
+			x - w/2, y + h/2, z - d/2,
+			x - w/2, y + h/2, z + d/2,
+			x + w/2, y + h/2, z + d/2,
+			x + w/2, y + h/2, z - d/2, 
+			faceColors[3][0], faceColors[3][1], faceColors[3][2]);
+		this.addQuad(
+			x - w/2, y - h/2, z - d/2,
+			x + w/2, y - h/2, z - d/2,
+			x + w/2, y + h/2, z - d/2,
+			x - w/2, y + h/2, z - d/2, 
+			faceColors[4][0], faceColors[4][1], faceColors[4][2]);
+		this.addQuad(
+			x - w/2, y - h/2, z + d/2,
+			x + w/2, y - h/2, z + d/2,
+			x + w/2, y + h/2, z + d/2,
+			x - w/2, y + h/2, z + d/2, 
+			faceColors[5][0], faceColors[5][1], faceColors[5][2]);
+	};
+	
+	WorldEditor.prototype.loadDataIntoProgram = function (shaderProgram, vBuffer, cBuffer, eBuffer) {
+		shaderProgram.addArrayBuffer(cBuffer, this.colors);
+		shaderProgram.addArrayBuffer(vBuffer, this.nodes);
+		shaderProgram.addIndexBuffer(eBuffer, this.elements);
+	};
   
 	XGL = {};
 	XGL.Program = Program;
+	XGL.ShapeEditor = WorldEditor;
 	XGL.loadGL = loadGL;	
 }) ();
