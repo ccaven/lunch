1d53a05bb159be9baa109556d31ba9d55bedfe0e
xacer <chacecaven@outlook.com>
2020-08-19 11:56:05 -0400

    Update main4.js

--- webgl/main4.js
+++ webgl/main4.js
@@ -71,6 +71,44 @@ var XGL;
 		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data), gl.STATIC_DRAW);
 		this.buffers[name] = buffer;
 	};
+	
+	Program.prototype.enableVertexAttrib = function (bufferName, attribName, numComponents, type, normalize, stride, offset) {
+		type = type || XGL.gl.FLOAT;
+		normalize = normalize || false;
+		stride = stride || 0;
+		offset = offset || 0;		
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers[bufferName]);
+		gl.vertexAttribPointer(this.attribLocations[attribName], numComponents, type, normalize, stride, offset);
+		gl.enableVertexAttribArray(this.attribLocations[attribName]);
+	};
+	
+	Program.prototype.enableElementBuffer = function (bufferName) {
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.buffers[bufferName]);
+	};
+	
+	Program.prototype.setUniform = function (uniformName, type, o1, o2, o3, o4) {
+		var loc = this.uniformLocations[uniformName];
+		if(type[0] === "1") {
+			gl["uniform" + type](loc, o1);
+		} else if (type[0] === "2") {
+			gl["uniform" + type](loc, o1, o2);
+		} else if (type[0] === "3") {
+			gl["uniform" + type](loc, o1, o2, o3);
+		} else {
+			gl["uniform" + type](loc, o1, o2, o3, o4);
+		}
+	};
+	
+	Program.prototype.setUniformMatrix = function (uniformName, type, data) {
+		gl["uniformMatrix" + type](this.uniformLocations[uniformName], false, data);
+	};
+	
+	Program.prototype.display = function (vertexCount, type, offset) {
+		offset = offset || 0;
+		type = type || gl.UNSIGNED_SHORT;
+		gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+	};
+	
 	XGL = {};
 	XGL.Program = Program;
 	XGL.loadGL = loadGL;	
